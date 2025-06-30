28242e9b314ce0988842fbafa45ba2b3b0d8b5bf
mohit <mohitkumaragarwal8@gmail.com>
2025-06-30 17:40:25 +0530

    finding embedding using spring ai

--- src/main/java/com/mohit/SpringAICode/OpenAIController.java
+++ src/main/java/com/mohit/SpringAICode/OpenAIController.java
@@ -1,24 +1,26 @@
 package com.mohit.SpringAICode;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.DefaultChatClientBuilder;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.SQLOutput;
 import java.util.Map;
 
 @RestController
 public class OpenAIController {
 
     private ChatClient chatClient;
+
+    @Autowired
+    private EmbeddingModel embeddingModel;
 
 //    public OpenAIController(OpenAiChatModel chatModel) {
 //        this.chatClient = ChatClient.create(chatModel);
@@ -72,6 +74,12 @@ public class OpenAIController {
                 .call()
                 .content();
         return response;
+    }
+
+    @PostMapping("api/embedding")
+    public float[] getembedding(@RequestParam String text)
+    {
+        return embeddingModel.embed(text);
     }
 
 

28f5986a7fff66692b78c24d46996cac26dc5338
mohit <mohitkumaragarwal8@gmail.com>
2025-07-03 11:39:30 +0530

    implement semantic search using TextSplitter and vector store

--- src/main/java/com/mohit/SpringAICode/OpenAIController.java
+++ src/main/java/com/mohit/SpringAICode/OpenAIController.java
@@ -7,11 +7,15 @@ import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -21,6 +25,9 @@ public class OpenAIController {
 
     @Autowired
     private EmbeddingModel embeddingModel;
+
+    @Autowired
+    private VectorStore vectorStore;
 
 //    public OpenAIController(OpenAiChatModel chatModel) {
 //        this.chatClient = ChatClient.create(chatModel);
@@ -98,6 +105,13 @@ public class OpenAIController {
         }
 
         return dotProduct*100 / (Math.sqrt(norm1) * Math.sqrt(norm2));
+    }
+
+    @PostMapping("api/product")
+    public List<Document> getProducts(@RequestParam String text)
+    {
+        //return vectorStore.similaritySearch(text);
+        return vectorStore.similaritySearch(SearchRequest.builder().query(text).topK(2).build());
     }
 
 

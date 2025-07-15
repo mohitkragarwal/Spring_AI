999553351b9cb626c23882b4f18b79a6e00092e5
mohit <mohitkumaragarwal8@gmail.com>
2025-07-15 07:17:50 +0530

    Implementing List Output Converter

---
+++ src/main/java/com/mohit/SpringAICode/controller/MovieConverter.java
@@ -0,0 +1,46 @@
+package com.mohit.SpringAICode.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+
+
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+public class MovieConverter {
+
+    private ChatClient chatClient;
+
+    public MovieConverter(OpenAiChatModel chatModel)
+    {
+        this.chatClient=ChatClient.create(chatModel);
+    }
+
+
+    @GetMapping("movies")
+    public List<String> getMovies(@RequestParam String name) {
+
+
+
+        List<String> movies = chatClient.prompt()
+                .user(u -> u.text("List top 10 movies of {name} based of imdb").param("name", name))
+                .call()
+                .entity(new ListOutputConverter(new DefaultConversionService()));
+
+
+        return movies;
+
+
+    }
+
+}
